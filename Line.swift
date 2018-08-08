cea74b107a9d25d2c1a591671dd14592ae055200
Drew Sullivan <dsullivan@dev-030416.local>
2018-08-08 16:17:06 -0400

    First real commit

---
+++ Line.swift
@@ -0,0 +1,15 @@
+//
+//  Line.swift
+//  TouchTracker
+//
+//  Created by Drew Sullivan on 8/8/18.
+//  Copyright © 2018 Drew Sullivan, DMA. All rights reserved.
+//
+
+import Foundation
+import CoreGraphics
+
+struct Line {
+    var begin = CGPoint.zero
+    var end = CGPoint.zero
+}
